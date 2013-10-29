92d65c78e99951f81e9ee04a6800c0de6f4d95ac
Roshan <Roshan@172.18.110.114>
2013-10-29 13:28:07 -0500

    i added some attribute and functionality to add event page. now this page is able to take input from the user which we can user store later, if we wish

--- BrandAmbassador/gen/com/example/brandambassador/R.java
+++ BrandAmbassador/gen/com/example/brandambassador/R.java
@@ -24,12 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
+        public static final int action_settings=0x7f08000a;
         public static final int addEventButton=0x7f080002;
         public static final int calendarButton=0x7f080001;
         public static final int documentButton=0x7f080006;
+        public static final int edit_message=0x7f080007;
         public static final int financeButton=0x7f080004;
+        public static final int location_message=0x7f080008;
         public static final int myCalendar=0x7f080000;
+        public static final int payrate_message=0x7f080009;
         public static final int reportButton=0x7f080005;
         public static final int selectedDate=0x7f080003;
     }
@@ -42,16 +45,26 @@ public final class R {
         public static final int main=0x7f070001;
     }
     public static final class string {
+        public static final int Addbutton=0x7f050009;
+        public static final int From=0x7f050010;
+        public static final int Location=0x7f05000e;
+        public static final int Title=0x7f05000c;
+        public static final int To=0x7f050011;
         public static final int action_settings=0x7f050001;
         public static final int addEvent=0x7f050007;
         public static final int app_name=0x7f050000;
         public static final int calendar_button=0x7f050002;
         public static final int documents_button=0x7f050003;
-        public static final int hello_world=0x7f050009;
+        public static final int hello_world=0x7f05000b;
+        public static final int location_message=0x7f05000f;
         public static final int money_button=0x7f050004;
+        public static final int pay_rate=0x7f050012;
+        public static final int payrate_message=0x7f050013;
         public static final int reports_button=0x7f050005;
         public static final int selectedDate=0x7f050006;
         public static final int title_activity_add_event=0x7f050008;
+        public static final int title_activity_display_event=0x7f05000a;
+        public static final int title_message=0x7f05000d;
     }
     public static final class style {
         /** 
